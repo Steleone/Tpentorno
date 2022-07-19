912e2de03c36afa1a7345fd636ff44ddb5d0a6c7
stefanoleoneredjar <68558343+stefanoleoneredjar@users.noreply.github.com>
2022-07-19 18:09:27 -0300

    allVowelsInWords

--- ejercicios/allVowelsInWords.sh
+++ ejercicios/allVowelsInWords.sh
@@ -3,10 +3,22 @@ while read line
   do
     for i in $line
       do
-        [[ "$i" =~ ^[A-Z]+$ ]] && words+=($i)
+        vars=()
+        check=0
+        cA=$(echo $i | grep -o -i "[aA]" | wc --lines)
+        [ $cA -ge 1 ] && check=$(($check+1)) && vars+=("a")
+        cE=$(echo $i | grep -o -i "[eE]" | wc --lines)
+        [ $cE -ge 1 ] && check=$(($check+1)) && vars+=("e")
+        cI=$(echo $i | grep -o -i "[iI]" | wc --lines)
+        [ $cI -ge 1 ] && check=$(($check+1)) && vars+=("i")
+        cO=$(echo $i | grep -o -i "[oO]" | wc --lines)
+        [ $cO -ge 1 ] && check=$(($check+1)) && vars+=("o")
+        cU=$(echo $i | grep -o -i "[uU]" | wc --lines)
+        [ $cU -ge 1 ] && check=$(($check+1)) && vars+=("u")
+        [ $check -eq 5 ] && words+=($i)
       done
 done < $1
 IFS=$'\n' sorted=($(sort <<<"${words[*]}"))
 unset IFS
-sorted=($(echo "${sorted[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
 printf "[%s]\n" "${sorted[@]}"
+#falta contar repeticiones
