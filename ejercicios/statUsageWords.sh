0bd4ff11c26be96d055e63b548139928d31f3659
Guillermo Mangiante <guillermomangiante@gmail.com>
2022-07-20 18:50:44 -0300

    Ejercicio 2 agregado al main

---
+++ ejercicios/statUsageWords.sh
@@ -0,0 +1,16 @@
+filename=$1
+[ -d $filename ] && echo "no existe el archivo" && exit 1
+list=()
+while read line
+do
+  for i in $line
+  do
+    [ ${#i} -ge 4 ] && list+=($i)
+  done
+done < $filename
+
+IFS=$'\n' sorted=($(sort <<<"${list[*]}" | uniq -c | sort -n )); unset IFS
+for value in "${sorted[@]}"
+do
+  echo $value
+done
