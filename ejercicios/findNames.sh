a59abf7e8df4e23133a6e073faf4f0d80ffd6126
Guillermo Mangiante <guillermomangiante@gmail.com>
2022-07-20 19:06:54 -0300

    Ejercicio3 guardado

--- ejercicios/findNames.sh
+++ ejercicios/findNames.sh
@@ -1,2 +1,18 @@
-name=$1
-[[ "$name" =~ ^[A-Z]{1}[a-z]*$ ]] && echo "si" || echo "no"
+filename=$1
+
+[ -d $filename ] && echo "no existe el archivo" && exit 1
+
+list=()
+
+while read line
+do
+    for i in $line
+    do
+    name=$i
+    [[ "$name" =~ ^[A-Z]{1}[a-z]*$ ]] && list+=($name) 
+    done
+done < $filename
+
+printf "[%s]\n" "${list[@]}"
+
+exit 0
