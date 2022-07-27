c82f80e14844ec46b262e52af340316905f70dae
Lucas Galvan <lucasgalvan706@gmail.com>
2022-07-27 19:27:21 -0300

    Corrección ejercicio 10

--- ejercicios/oneVowelWords.sh
+++ ejercicios/oneVowelWords.sh
@@ -11,15 +11,15 @@ while read line
         [ $len -lt 4 ] && continue
         vars=()
         check=0
-        cA=$(echo $i | grep -o -i "[aA]" | wc --lines)
+        cA=$(echo $i | grep -o -i "[aA]" | wc -l)
         [ $cA -ge 1 ] && check=$(($check+1)) && vars+=("a")
-        cE=$(echo $i | grep -o -i "[eE]" | wc --lines)
+        cE=$(echo $i | grep -o -i "[eE]" | wc -l)
         [ $cE -ge 1 ] && check=$(($check+1)) && vars+=("e")
-        cI=$(echo $i | grep -o -i "[iI]" | wc --lines)
+        cI=$(echo $i | grep -o -i "[iI]" | wc -l)
         [ $cI -ge 1 ] && check=$(($check+1)) && vars+=("i")
-        cO=$(echo $i | grep -o -i "[oO]" | wc --lines)
+        cO=$(echo $i | grep -o -i "[oO]" | wc -l)
         [ $cO -ge 1 ] && check=$(($check+1)) && vars+=("o")
-        cU=$(echo $i | grep -o -i "[uU]" | wc --lines)
+        cU=$(echo $i | grep -o -i "[uU]" | wc -l)
         [ $cU -ge 1 ] && check=$(($check+1)) && vars+=("u")
         [ $check -eq 1 ] && words+=($i)
       done
