4b1960d14425779fcbd24650a83ac501fa3b44b6
Steleone <stefanoleone20@gmail.com>
2022-07-28 15:36:42 -0300

    Eliminado sorted no usado

--- ejercicios/allUpperCase.sh
+++ ejercicios/allUpperCase.sh
@@ -9,7 +9,5 @@ while read line
         [[ "$i" =~ ^[A-Z]+$ ]] && words+=($i)
       done
 done < $1
-IFS=$'\n' sorted=($(sort <<<"${words[*]}"))
-unset IFS
 sorted=($(echo "${sorted[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
 printf "[%s]\n" "${sorted[@]}"
